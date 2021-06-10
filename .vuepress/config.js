340b6136c50fbda2c097d9fa2e4b2cb5ef358380
tian_jianen <tian_jianen@venusgroup.com.cn>
2021-06-10 17:59:27 +0800

    修改meta错误

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -27,8 +27,8 @@ module.exports = {
     ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
     ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
     ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
-    ['meta', { name: "google-site-verification", content: "NArLfFpk5oX0iEsmzxlFGCk6iyFH9MOmyTxk5k_rrno" }]
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['meta', { name: 'google-site-verification', content: 'NArLfFpk5oX0iEsmzxlFGCk6iyFH9MOmyTxk5k_rrno' }]
   ],
   plugins: {
     '@vuepress/pwa': {
