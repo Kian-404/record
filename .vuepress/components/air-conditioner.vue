a9ec673b2ae7fe06872d6739774d7e7535e8b30f
tian_jianen <tian_jianen@venusgroup.com.cn>
2021-06-16 10:41:47 +0800

    添加云空调组件

---
+++ .vuepress/components/air-conditioner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="conditioner">
+    <iframe class="iframe" src="https://ac.yunyoujun.cn"></iframe>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.conditioner{
+ 
+}
+.conditioner .iframe{
+ width: 100%;
+  height: 800px;
+  border: none;
+}
+</style>
---
+++ docs/fontend/air-conditioner.md
@@ -0,0 +1,15 @@
+---
+title: 云空调
+date: 2021-05-25
+sticky: 1
+---
+
+:::tip
+
+超省电云空调 随手关门
+
+:::
+
+<!-- more -->
+
+<air-conditioner/>
