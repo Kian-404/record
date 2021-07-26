77abc0f0a59ae0ded8fcd6c1cf93d748e8836b05
tian_jianen <tian_jianen@venusgroup.com.cn>
2021-07-26 10:54:02 +0800

    添加kinetic-loader

---
+++ .vuepress/components/kinetic-loader.vue
@@ -0,0 +1,87 @@
+<!--
+ * @Version: 0.0.0.1
+ * @LastEditor: Kian
+ * @Date: 2021-07-26 10:46:00
+ * @LastEditTime: 2021-07-26 10:51:59
+-->
+<template>
+  <div class="kinetic-loader">
+    <div class="kinetic"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+* {
+  box-sizing: border-box;
+}
+
+.kinetic-loader {
+  background-color: #2c3e50;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 60vh;
+  overflow: hidden;
+  margin: 0;
+}
+
+.kinetic {
+  position: relative;
+  height: 80px;
+  width: 80px;
+}
+
+.kinetic::after,
+.kinetic::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 0;
+  border: 50px solid transparent;
+  border-bottom-color: #fff;
+  animation: rotateA 2s linear infinite 0.5s;
+}
+
+.kinetic::before {
+  transform: rotate(90deg);
+  animation: rotateB 2s linear infinite;
+}
+
+@keyframes rotateA {
+  0%,
+  25% {
+    transform: rotate(0deg);
+  }
+
+  50%,
+  75% {
+    transform: rotate(180deg);
+  }
+
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+@keyframes rotateB {
+  0%,
+  25% {
+    transform: rotate(90deg);
+  }
+
+  50%,
+  75% {
+    transform: rotate(270deg);
+  }
+
+  100% {
+    transform: rotate(450deg);
+  }
+}
+</style>
---
+++ docs/fontend/50projects/kinetic-loader.md
@@ -0,0 +1,33 @@
+---
+title: kinetic-loader
+categories: 
+ - frontend
+date: 2021-07-26
+---
+
+:::tip
+
+50 project 50 days practice
+
+kinetic-loader
+
+:::
+
+
+<!-- more -->
+
+<kinetic-loader/>
+
+-------
+
+<RecoDemo :collapse="false">
+  <template slot="code-template">
+    <<< @/.vuepress/components/kinetic-loader.vue?template
+  </template>
+  <template slot="code-script">
+    <<< @/.vuepress/components/kinetic-loader.vue?script
+  </template>
+  <template slot="code-style">
+    <<< @/.vuepress/components/kinetic-loader.vue?style
+  </template>
+</RecoDemo>
